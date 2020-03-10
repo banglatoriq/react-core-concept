cfce3ab2f35db65488b4391e3695f6a6c30678bc
TORIQ <tariqulislambd01@gmail.com>
2020-03-11 00:57:23 +0600

    10 mar 2020

--- src/App.js
+++ src/App.js
@@ -3,17 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+ 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit Done <code>src/App.js</code> and save to reload.
-        </p>
-        
+        <p>I want to be a react app developer</p>
+        <Person name="Rayhan" profession="designer"></Person>
+        <Person name="Ibrahim" profession="Developer"></Person>
+        <Person name="Arif" profession="Developer"></Person>
+        <Person name="Sakib" profession="Banker"></Person>
       </header>
     </div>
   );
 }
+function Person(props){
+  const styleComponent={
+    border:'2px solid gold',
+    padding:'5px',
+    margin:'3px'
+  }
+  return(
+    <div style={styleComponent}>
+      <h1>Name: {props.name}</h1>
+      <p>Profession: {props.profession}</p>
+    </div>
+  )
+}
+
 
 export default App;
